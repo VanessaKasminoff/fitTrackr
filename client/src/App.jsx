8f420f6b9b2988649a4ff8bf366ca808d67264e4
ChaseHarding <chaseharding965@gmail.com>
2024-06-18 21:13:25 -0700

    reorganized previous html changes to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,21 @@ import './assets/css/App.css'
 function App() {
   return (
     <>
-      <h1>Hi! Welcome to fitTrackr!</h1>
+      <header class="header">
+        {/* Logo */}
+        <div class="logo-box">
+          <img src="#" alt="logo" class="logo" />
+        </div>
+
+        {/* Hero Content */}
+        <div class="text-box">
+          <h1 class="heading-primary">
+            <span></span>
+            <span></span>
+          </h1>
+        </div>
+
+      </header>
     </>
   )
 }
