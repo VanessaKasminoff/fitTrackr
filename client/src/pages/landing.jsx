36b284afc95409cd62b2c2440627aaa1d9023d2c
Erin Y Kim <erinykim04@gmail.com>
2024-06-24 23:23:22 -0700

    added login/signup structure to my jsx

--- client/src/pages/landing.jsx
+++ client/src/pages/landing.jsx
@@ -2,13 +2,39 @@ import { Link } from "react-router-dom";
 
 function Landing() {
   return (
-    <div>
-      <h1>Landing Page!</h1>
-      <div>
-        <Link to="/NewUser">New User</Link>
-      </div>
-      <Link to="/LogIn">Log In</Link>
+    <header class="header">
+    {/* nav */}
+    <nav>
+      <input type="checkbox" id="check" />
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <ul>
+        <li><a class="active" href="#"></a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    {/* Logo */}
+    <div class="logo-box">
+      <img src="src/assets/img/logo.png" alt="logo" class="logo" />
     </div>
+    {/* Hero Content */}
+    <div class="text-box">
+      <h1 class="heading-primary">
+        <span className="heading-primary-main">FitTrackr</span>
+        <span className="heading-primary-sub">Empower your fitness journey</span>
+      </h1>
+    </div>
+  </header>
+    // <div>
+    //   <h1>Landing Page!</h1>
+    //   <div>
+    //   <Link to="/NewUser">New User</Link>
+    //   </div>
+    //   <Link to="/LogIn">Log In</Link>
+    // </div>
   );
 }
 
