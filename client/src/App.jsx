d61322bcca1b214c3c3368cdfdf02d3091b12879
ChaseHarding <chaseharding965@gmail.com>
2024-06-24 20:39:02 -0700

    Added responsive nav bar design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,19 @@ function App() {
   return (
     <>
       <header class="header">
+        {/* nav */}
+        <nav>
+          <input type="checkbox" id="check" />
+          <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+          </label>
+          <ul>
+            <li><a class="active" href="#"></a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
         {/* Logo */}
         <div class="logo-box">
           <img src="src/assets/img/logo.png" alt="logo" class="logo" />
