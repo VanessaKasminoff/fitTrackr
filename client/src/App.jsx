f9e04fed15d7f3c047f841a96ca8a4288d5248ce
Ghostbitstudios <126435713+Ghostbitstudios@users.noreply.github.com>
2024-06-24 17:52:49 -0700

    made 4 routes work! and added error page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,23 +5,26 @@ import Landing from "./pages/landing";
 import NewUser from "./pages/NewUser";
 import LogIn from "./pages/LogIn";
 import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
+    // put anything before the <BrowserRouter> and it will appear in all webpages! so like nav bar or logo is what im thinking of, make sure you wrap everuthing into a div if you are going to! this includes <BrowserRouter>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/NewUser" element={<NewUser />} />
         <Route path="/LogIn" element={<LogIn />} />
         <Route path="/Home" element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
-  return (
-    <>
-      <h1>Hi! Welcome to fitTrackr!</h1>
-    </>
-  );
+  // return (
+  //   <>
+  //     <h1>Hi! Welcome to fitTrackr!</h1>
+  //   </>
+  // );
 }
 
 export default App;
