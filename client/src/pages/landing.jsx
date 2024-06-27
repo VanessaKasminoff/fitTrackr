91a8ed340bc313f3e477c7000543d3e5819724c1
Erin Y Kim <erinykim04@gmail.com>
2024-06-26 21:16:48 -0700

    added gif to landing! changed flex end and fixed pathing to sass.

--- client/src/pages/landing.jsx
+++ client/src/pages/landing.jsx
@@ -39,15 +39,7 @@ function Landing() {
         {/* Add your other sections here */}
       </main>
     </div>
-  </header>
-    // <div>
-    //   <h1>Landing Page!</h1>
-    //   <div>
-    //   <Link to="/NewUser">New User</Link>
-    //   </div>
-    //   <Link to="/LogIn">Log In</Link>
-    // </div>
   );
-}
+};
 
 export default Landing;
