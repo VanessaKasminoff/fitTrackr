95adf7d39ddf289a804279fcf0b5867bad2818ed
ChaseHarding <chaseharding965@gmail.com>
2024-06-26 20:24:34 -0700

    formatted card content

--- client/src/assets/components/landingCard.jsx
+++ client/src/assets/components/landingCard.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import landingPageCard from '../scss/_landingPageCard.scss';
+<link rel="stylesheet" href="style.css" />
+
+import chartImage from '../img/kickboxing.jpg';
+import appleWatchImage from '../img/appleWatch.png';
+import graphImage from '../img/graph.png';
 
 const cardData = [
-    { title: 'card 1', content: 'Content for card 1', imageUrl: '' },
-    { title: 'card 2', content: 'Content for card 2', imageUrl: '' },
-    { title: 'card 3', content: 'Content for card 3', imageUrl: '' }
+    { title: 'card 1', content: 'Content for card 1', imageUrl: chartImage },
+    { title: 'card 2', content: 'Content for card 2', imageUrl: appleWatchImage },
+    { title: 'card 3', content: 'Content for card 3', imageUrl: graphImage }
 ];
 
-const Card = ({ title, content }) => (
+const Card = ({ title, content, imageUrl }) => (
     <div className="card">
         <div className='card-text'>
             <h2 className='card-title'>{title}</h2>
@@ -22,7 +26,7 @@ const Card = ({ title, content }) => (
 const CardContainer = () => (
     <div id="cardContainer">
         {cardData.map((item, index) => (
-            <Card key={index} title={item.title} content={item.content} />
+            <Card key={index} title={item.title} content={item.content} imageUrl={item.imageUrl} />
         ))}
     </div>
 );
