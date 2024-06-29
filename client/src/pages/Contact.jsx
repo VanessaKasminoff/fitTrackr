d0b994a3ed2af644f07266e7da1c52267895e55d
ChaseHarding <chaseharding965@gmail.com>
2024-06-28 23:42:52 -0700

    added to the contact page, created a card component for conact information.

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,5 +1,22 @@
+import React from "react";
+import contactCard from "../assets/components/contactCard";
+
 function Contact() {
-  return <div>Contact Us</div>;
+  return (
+    <div className="contact-page">
+    <header className="contact-header">
+      <div className="hero-image">
+        <div className="hero-overlay">
+          <div className="cards-container">
+            <contactCard title="Find Us" content="123 FitTrackr Way, Las Vegas, NV"/>
+            <contactCard title="Hours" content="Mon-Fri 9am - 5pm"/>
+            <contactCard title="Call Us" content="(123) 456-789"/>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
+  );
 }
 
 export default Contact;
