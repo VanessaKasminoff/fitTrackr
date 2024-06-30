7fd8f75728f0df8dcba270d380d7fe2de25b05a8
Ghostbitstudios <126435713+Ghostbitstudios@users.noreply.github.com>
2024-06-29 17:18:18 -0700

    managed to display all workouts in a neat little containers

--- client/src/pages/landing.jsx
+++ client/src/pages/landing.jsx
@@ -1,45 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import CardContainer from '../assets/components/landingCard'; 
+import { Link } from "react-router-dom";
 
 function Landing() {
   return (
     <div>
-      <header className="header">
-        {/* nav */}
-        <nav>
-          <input type="checkbox" id="check" />
-          <label htmlFor="check" className="checkbtn">
-            <i className="fas fa-bars"></i>
-          </label>
-          <label className="logo-box">
-            <img src="src/assets/img/logo.png" alt="logo" className="logo" />
-          </label>
-          <ul>
-            <li><a className="active" href="#"></a></li>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Login</a></li>
-          </ul>
-        </nav>
-        {/* Hero Content */}
-        <div className="text-box">
-          <h1 className="heading-primary">
-            <span className="heading-primary-main">FitTrackr</span>
-            <span className="heading-primary-sub">Empower your fitness journey</span>
-          </h1>
-        </div>
-      </header>
-      <main className="main-content">
-        <section className="">
-          
-        </section>
-        <CardContainer />
-        {/* Add your other sections here */}
-      </main>
+      <h1>Landing Page!</h1>
+      <div>
+        <Link to="/NewUser">New User</Link>
+      </div>
+      <Link to="/LogIn">Log In</Link>
     </div>
   );
-};
+}
 
 export default Landing;
