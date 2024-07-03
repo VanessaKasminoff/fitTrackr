59327a0acb12a0e89d16a6df52a899797d808b72
ChaseHarding <chaseharding965@gmail.com>
2024-07-02 22:33:47 -0700

    created a recipe and tool component for meal prep page

---
+++ client/src/assets/components/MealPrepRecipe.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function MealPrepRecipe() {
+    return (
+        <section className="mealprep-recipes">
+            <h2>Recipes</h2>
+            <div className="recipe">
+                <h3>Breakfast Smoothie</h3>
+                <p>A quick and easy smoothie recipe to start your day right.</p>
+            </div>
+            <div className="recipe">
+                <h3>Chicken and Veggie Stir-Fry</h3>
+                <p>A falvorful and healthy stir-fry that's perfect for meal prep</p>
+            </div>
+        </section>
+    );
+}
+
+export default MealPrepRecipe;
