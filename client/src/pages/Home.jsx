3923d4c89e939f52cbe69109d4f5d68b9e8c03fd
Vanessa Kasminoff <sunnset07@gmail.com>
2024-07-02 22:54:38 -0700

    Added greeting div for logged in user on dashboard. Created script in server package to seed exercises data

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,16 +7,30 @@ import DashboardCoachingCard from "../assets/components/DashboardCoachingCard";
 import dashBoy from "../assets/img/stayactive.png";
 import { CurrentUser } from '../contexts/CurrentUser';
 
-function Home() { const { currentUser } = useContext(CurrentUser)
+function Home() { 
+  const { currentUser } = useContext(CurrentUser)
+
+  let greetingDiv;
+
+  if (currentUser) {
+    greetingDiv = (
+      <div className="main-card">
+        <DashboardCard title="Main Card" content={`Hello ${currentUser.firstName}`} />
+      </div>
+    )
+  }
+
   return (
     <div className="home-page">
       <header className="home-header">
       <h1>Welcome to your Dashboard</h1>
       </header>
       <main className="home-main">
+{/* 
         <div className="main-card">
           <DashboardCard title="Main Card" content={`Hello ${currentUser.firstName}`} />
-        </div>
+        </div> */}
+        {greetingDiv}
         <div className="diet-cards">
           <DashboardCard title="Diet card 1" content="This is diet card 1 info" />
           <DashboardCard title="Diet card 2" content="This is diet card 2 info" />
