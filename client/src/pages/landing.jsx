1e4dca9739efe46aabacf1bd61913f0f9984bb01
ChaseHarding <chaseharding965@gmail.com>
2024-07-02 23:16:44 -0700

    added link to contact page in nav

--- client/src/pages/landing.jsx
+++ client/src/pages/landing.jsx
@@ -20,7 +20,7 @@ function Landing() {
             <li><a className="active" href="#"></a></li>
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
+            <li><a href="/contact">Contact</a></li>
             <li><a href="/LogIn">Login</a></li>
           </ul>
         </nav>
