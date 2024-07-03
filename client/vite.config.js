20af760b6e6085ae70163350df43d1470c51ec24
Vanessa Kasminoff <sunnset07@gmail.com>
2024-07-02 19:25:00 -0700

    Updated config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3003",
+      "/api": "http://localhost:3008",
     },
   },
   plugins: [react()],
