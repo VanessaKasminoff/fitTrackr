9bbc63de5b0db4228921c3a9960f3502c2062f0c
ChaseHarding <chaseharding965@gmail.com>
2024-07-02 22:54:08 -0700

    styled mealprep page

--- client/src/pages/MealPrep.jsx
+++ client/src/pages/MealPrep.jsx
@@ -1,4 +1,9 @@
 import '../assets/scss/_dashboardCard.scss';
+import MealPrepGuide from '../assets/components/MealPrepGuide';
+import MealPrepPlan from '../assets/components/MealPrepPlan';
+import MealPrepRecipe from '../assets/components/MealPrepRecipe';
+import MealPrepTool from '../assets/components/MealPrepTool';
+import MealPrepFAQ from '../assets/components/MealPrepFAQ';
 
 function MealPrep() {
   return (
@@ -7,13 +12,11 @@ function MealPrep() {
         <h1>Meal Prep</h1>
         <p>Custom meal plans to fuel your fitness journey</p>
       </header>
-      <section className='mealprep-intro'>
-        <h2>Why Meal Prep?</h2>
-        <p>Meal prepping is a crucial part of a successful fitness journey. It helps you stay on track with your nutrition goals, save time, and reduce stress during the week</p>
-      </section>
-      <section className="mealprep-content">
-
-      </section>
+      <MealPrepGuide />
+      <MealPrepPlan />
+      <MealPrepRecipe />
+      <MealPrepTool />
+      <MealPrepFAQ />
     </div>
   )
   
