a22115100b61a4e673be5b8afefef1fe8cabb427
ChaseHarding <chaseharding965@gmail.com>
2024-07-02 22:18:48 -0700

    styled the meal prep page, created guide and plan components for meal prep page

--- client/src/pages/MealPrep.jsx
+++ client/src/pages/MealPrep.jsx
@@ -1,5 +1,22 @@
-function Landing() {
-  return <div>Landing</div>;
+import '../assets/scss/_dashboardCard.scss';
+
+function MealPrep() {
+  return (
+    <div className="mealprep-page">
+      <header className="mealprep-header">
+        <h1>Meal Prep</h1>
+        <p>Custom meal plans to fuel your fitness journey</p>
+      </header>
+      <section className='mealprep-intro'>
+        <h2>Why Meal Prep?</h2>
+        <p>Meal prepping is a crucial part of a successful fitness journey. It helps you stay on track with your nutrition goals, save time, and reduce stress during the week</p>
+      </section>
+      <section className="mealprep-content">
+
+      </section>
+    </div>
+  )
+  
 }
 
-export default Landing;
+export default MealPrep;
