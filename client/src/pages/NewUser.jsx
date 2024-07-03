f47a89ed36fde285b21c38094f83b4d718f02fb1
Erin Y Kim <erinykim04@gmail.com>
2024-07-02 19:14:50 -0700

    Changed graph to gif and fixed sizing of card container

--- client/src/pages/NewUser.jsx
+++ client/src/pages/NewUser.jsx
@@ -78,64 +78,9 @@ const SignUp = () => {
             <Link to="/Home"><button type="button">Sign Up</button></Link>
           </form>
         </div>
+        </div>
+        </div>
 );
-
-//   return (
-//     <div>
-//       <h1>Log In!</h1>
-//       <form onSubmit={handleSubmit}>
-//         <label>First Name</label>
-//         <input
-//           value={user.firstName}
-//           onChange={e => setUser({...user, firstName: e.target.value})}
-//           id="firstName"
-//           name="firstName"
-//           required
-//         />
-//         <label>Last Name</label>
-//         <input
-//           value={user.lastName}
-//           onChange={e => setUser({...user, lastName: e.target.value})}
-//           id="lastName"
-//           name="lastName"
-//           required
-//         />
-//         <label>Email</label>
-//         <input
-//           value={user.email}
-//           onChange={e => setUser({...user, email: e.target.value})}
-//           id="email"
-//           name="email"
-//           required
-//         />
-//         <label>Gender</label>
-//         <input
-//           value={user.gender}
-//           onChange={e => setUser({...user, gender: e.target.value})}
-//           id="gender"
-//           name="gender"
-//           required
-//         />
-//         <label>Password</label>
-//         <input
-//           type="password"
-//           value={user.password}
-//           onChange={e => setUser({...user, password: e.target.value})}
-//           id="password"
-//           name="password"
-//           required
-//         />
-//         <input
-//           type="submit"
-//           value='Sign Up'
-//         />
-//       </form>
-//       <div>
-//         <Link to="/Home">Create user</Link>
-//       </div>
-//     </div>
-//   );
-
 };
 
 export default SignUp;
